ce3aac0e9420a1ce7090ae9ffbbff70e12ba7eb9
Pavel Sanchez <pavel@bluedot.dev>
2020-01-20 18:30:35 -0500

    :ok_hand: Github Finder Project

--- Code/8 - API Project/Github Finder/app.js
+++ Code/8 - API Project/Github Finder/app.js
@@ -1,1 +1,31 @@
-console.log('App');
+// __INIT Github
+const github = new Github();
+
+// __INIT UI
+const ui = new UI();
+
+// __Searcg input
+const searchUser = document.getElementById('searchUser');
+
+searchUser.addEventListener('keyup', function(e) {
+	// __Get input test
+	const userText = e.target.value;
+
+	if (userText !== '') {
+		github.getUser(userText).then(data => {
+			if (data.profile.message === 'Not Found') {
+				// __Show alert
+				setTimeout(() => {
+					ui.showAlert('User not found', 'alert alert-danger');
+				}, 500);
+			} else {
+				// __Show profile
+				ui.showProfile(data.profile);
+				ui.showRepos(data.repos);
+			}
+		});
+	} else {
+		// __Clear profile
+		ui.clearProfile();
+	}
+});
--- Code/8 - API Project/Github Finder/github.js
+++ Code/8 - API Project/Github Finder/github.js
@@ -1,1 +1,26 @@
-console.log('Github');
+class Github {
+	constructor() {
+		this.client_id = 'b3565224bc81b044643a';
+		this.client_secret = 'a8ce181f30890c426d218a48c0be96d65ce0bdee';
+		this.repos_count = 5;
+		this.repos_sort = 'created: asc';
+	}
+
+	async getUser(user) {
+		const profileResponse = await fetch(
+			`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+		);
+
+		const repoResponse = await fetch(
+			`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+		);
+
+		const profile = await profileResponse.json();
+		const repos = await repoResponse.json();
+
+		return {
+			profile,
+			repos,
+		};
+	}
+}
